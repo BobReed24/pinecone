ea7572a7dacbedc091a585655627e7aebaf29da9
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-01-29 12:35:20 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ cd $HOME/sys/
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pine.py
 cd bin
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/upgrade.sh
+chmod +x upgrade.sh
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/restarters/restart-sys-soft.sh
 mv restart-sys-soft.sh restarters/restart-sys-soft.sh
+cd restarters
+chmod +x restart-sys-soft.sh
 cd $main
 python $HOME/sys/command_pine.py
