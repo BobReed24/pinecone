c1371d99199c678525a62ee0c4b085a55aaea157
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-01-29 11:43:41 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+mkdir $HOME/sys
+mkdir $HOME/sys/bin
+mkdir $HOME/sys/bin/restarters
+cd $HOME/sys/
+wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pine.py
+cd bin
+wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/upgrade.sh
+wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/restarters/restart-sys-soft.sh
+mv restart-sys-soft.sh restarters/restart-sys-soft.sh
+cd $main
+python $HOME/sys/command_pine.py
