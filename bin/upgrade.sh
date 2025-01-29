9b9791fd382c754ab1ff73feca459eae7558d3f0
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-01-29 12:36:34 -0600

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 tmux kill-session -t upgrade-sys-4329482390859056203903423490
 tmux new-session -d -s upgrade-sys-4329482390859056203903423490 'rm $HOME/sys/command_pine.py && wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pine.py && mv command_pine.py $HOME/sys/command_pine.py'
 sleep 3
