92f41bba5baba0fd37b3ea6c69b1005542416d80
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-01-29 12:50:36 -0600

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -2,4 +2,4 @@
 tmux kill-session -t upgrade-sys-4329482390859056203903423490
 tmux new-session -d -s upgrade-sys-4329482390859056203903423490 'rm $HOME/sys/command_pine.py && wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pine.py && mv command_pine.py $HOME/sys/command_pine.py'
 sleep 3
-bash $HOME/sys/bin/restarters/restart-sys-soft.sh
+sh $HOME/sys/bin/restarters/restart-sys-soft.sh
