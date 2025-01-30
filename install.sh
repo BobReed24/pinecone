f444c11207ad6c881896683a0bf3b89a237e53cb
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-01-30 11:12:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,17 @@ mkdir $HOME/sys/bin
 mkdir $HOME/sys/bin/restarters
 cd $HOME/sys/
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pinecone
+chmod +x command_pinecone
 cd bin
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/upgrade.sh
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/restarters/restart-sys-soft.sh
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/build.sh
 mv restart-sys-soft.sh restarters/restart-sys-soft.sh
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/pinecone
+chmod +x pinecone
+sudo mv pinecone /bin/pinecone
 cd restarters
 cd $main
+# sudo sh $HOME/sys/bin/build.sh
 clear
 echo "Done!"
