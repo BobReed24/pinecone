252d4beab914915f3c5ec9d7b920d2ca46e9d1c8
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-03 08:19:40 -0600

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-tmux kill-session -t upgrade-sys-4329482390859056203903423490
-tmux new-session -d -s upgrade-sys-4329482390859056203903423490 'rm $HOME/sys/command_pinecone && wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pinecone && chmod +x command_pinecone && mv command_pinecone $HOME/sys/command_pinecone'
+tmux new-session -d -s upgrade-sys-4329482390859056203903423490 'rm $HOME/pinecone/command_pinecone && wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pinecone && chmod +x command_pinecone && mv command_pinecone $HOME/pinecone/command_pinecone'
 sleep 3
-sh $HOME/sys/bin/restarters/restart-sys-soft.sh
+sh $HOME/pinecone/bin/restarters/restart-sys-soft.sh
+tmux kill-session -t upgrade-sys-4329482390859056203903423490
