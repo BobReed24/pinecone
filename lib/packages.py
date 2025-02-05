078c9f917e0c3d47264c65e36978169b0538be82
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-05 12:43:02 -0600

    Update packages.py

--- lib/packages.py
+++ lib/packages.py
@@ -1,4 +1,4 @@
 packages = [
-    "help", "https://raw.githubusercontent.com/BobReed24/pinecone/refs/heads/main/lib/help.py",
-
+    "http-server", "https://github.com/BobReed24/pinecone-libs/raw/refs/heads/main/http-server.py", "flask",
+    "help", "https://raw.githubusercontent.com/BobReed24/pinecone/refs/heads/main/lib/help.py", "termcolor",
 ]
