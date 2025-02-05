7c92a5069861184142d88e7f0e4d69a63792e930
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-05 08:41:59 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR='tee -a' visudo
 sudo apt-get -y install tmux
 sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
 pip install termcolor
 sudo mkdir /pinecone
 sudo mkdir /pinecone/bin
