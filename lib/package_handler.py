ff0585ec0cc54ae6daa533a9037c4b3f242f388b
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-13 12:47:43 -0600

    Update package_handler.py

--- lib/package_handler.py
+++ lib/package_handler.py
@@ -53,7 +53,7 @@ class PackageHandler:
             print(colored("Error: Cannot remove package_handler and package list", 'red'))
 
     def list_installed(self):
-        installed_packages = [f[:-3] for f in os.listdir("/pinecone/lib") if f.endswith(".py")]
+        installed_packages = [f[:-3] for f in os.listdir("/pinecone/lib/") if f.endswith(".py")]
         if installed_packages:
             print("Installed packages:")
             for pkg in installed_packages:
