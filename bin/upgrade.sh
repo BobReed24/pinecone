f10a50241ef283f200f5b84b5dde753ac300294f
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-13 11:16:32 -0600

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,5 +1,5 @@
-rm pinecone/command_pinecone /pinecone/lib/package_handler.py /pinecone/lib/packages.py /pinecone/lib/__init__.py 
-wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/command_pinecone && mv command_pinecone /pinecone/command_pinecone
-wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/package_handler.py && mv package_handler.py /pinecone/lib/package_handler.py
-wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/packages.py && mv packages.py /pinecone/lib/packages.py
-wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/__init__.py && mv __init__.py /pinecone/lib/__init__.py
+sudo rm pinecone/command_pinecone /pinecone/lib/package_handler.py /pinecone/lib/packages.py /pinecone/lib/__init__.py 
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/command_pinecone && sudo mv command_pinecone /pinecone/command_pinecone
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/package_handler.py && sudo mv package_handler.py /pinecone/lib/package_handler.py
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/packages.py && sudo mv packages.py /pinecone/lib/packages.py
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/__init__.py && sudo mv __init__.py /pinecone/lib/__init__.py
