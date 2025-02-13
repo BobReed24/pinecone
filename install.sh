2e68c98c245a8a15661db3ea7192ceed343ceb7e
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-13 11:32:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo mkdir /pinecone
 sudo mkdir /pinecone/bin
 sudo mkdir /pinecone/bin/restarters
 wget https://www.dropbox.com/scl/fi/0o09zv4tdcltvlxs67d7l/command_pinecone?rlkey=zp3n9y3z87nk1ip2wpbi731ej&st=5k1lickd&dl=1
+sleep 4
 sudo mv 'command_pinecone?rlkey=zp3n9y3z87nk1ip2wpbi731ej' /pinecone/command_pinecone
 sudo chmod +x /pinecone/command_pinecone
 wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/bin/upgrade.sh
