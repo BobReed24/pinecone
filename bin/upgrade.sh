4e41753b9ee05c3de8165916a6eb33e09b692506
Hunkon <153524469+BobReed24@users.noreply.github.com>
2025-02-13 11:07:39 -0600

    Update upgrade.sh

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-tmux new-session -d -s upgrade-sys-4329482390859056203903423490 'sudo rm /pinecone/command_pinecone && wget https://raw.githubusercontent.com/BobReed24/hunkon-os/refs/heads/main/command_pinecone && sudo chmod +x command_pinecone && sudo mv command_pinecone /pinecone/command_pinecone'
-sleep 3
-sh /pinecone/bin/restarters/restart-sys-soft.sh
-tmux kill-session -t upgrade-sys-4329482390859056203903423490
+rm pinecone/command_pinecone /pinecone/lib/package_handler.py /pinecone/lib/packages.py /pinecone/lib/__init__.py 
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/command_pinecone && mv command_pinecone /pinecone/command_pinecone
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/package_handler.py && mv package_handler.py /pinecone/lib/package_handler.py
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/packages.py && mv packages.py /pinecone/lib/packages.py
+wget https://github.com/BobReed24/pinecone/raw/refs/heads/main/lib/__init__.py && mv __init__.py /pinecone/lib/__init__.py
