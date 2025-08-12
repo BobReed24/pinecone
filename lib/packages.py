e5fdf7c5282a45e5cac45e2745977fc61c0084f9
Alex Matzen <153524469+BobReed24@users.noreply.github.com>
2025-08-12 13:17:43 -0500

    Update packages.py

--- lib/packages.py
+++ lib/packages.py
@@ -1,7 +1,7 @@
 link = "https://github.com/BobReed24/pinecone-libs/raw/refs/heads/main/"
 packages = [
     "http-server", "https://github.com/BobReed24/pinecone-libs/raw/refs/heads/main/http-server.py", "flask", "V0.1.0",
-    "help", "https://raw.githubusercontent.com/BobReed24/pinecone/refs/heads/main/lib/help.py", "termcolor", "V0.1.6",
+    "help", "https://raw.githubusercontent.com/BobReed24/pinecone/refs/heads/main/lib/help.py", "termcolor", "V0.1.9",
     "battery", f"{link}battery.py", "psutil", "V1.0.0",
     "drsize", f"{link}drsize.py", "os", "V1.0.0"
 ]
